268ccff46be15c6b640bdf2a8f8858f451cabeca
Eugene Zorin <112591311+EugeneZorin@users.noreply.github.com>
2022-12-24 20:02:55 +0300

    Version: 0.00.1 Adding: 1) created the first version of the main screen

--- app/src/main/java/com/example/casemanager/MainActivity.kt
+++ app/src/main/java/com/example/casemanager/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.casemanager
 
-import android.graphics.Color.parseColor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,13 +8,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
